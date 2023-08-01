934b3e7c7a902ad0363316bea37df43e4ff28080
Anmol Kapil <aanmolkapil@gmail.com>
2023-08-01 13:10:30 +0530

    Create Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+const Sidebar:React.FC = () =>{
+  return (
+    <div>
+      <h1 className='text-4xl'>Search Results</h1>
+      
+    </div>
+  )
+}
+
+export default Sidebar;
--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,12 +1,17 @@
+import Sidebar from '../components/Sidebar';
 import Product from '../components/Product';
 import image from '../assets/linen jumpsuit.jpeg';
 
 const SearchResult:React.FC = () => {
   return (
+    <div className='flex'>
+
+    <Sidebar/>
     <div className='grid grid-cols-4'>
         
         <Product productImage={image} productName='Linen jumpsuit' price={500} discountedPrice={300} stars={4} ratingCount={200}/>
 
+    </div>
     </div>
   )
 }
