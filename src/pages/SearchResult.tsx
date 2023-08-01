5ef665c2ebec60d788d62a5604e9175e653284fa
Anmol Kapil <aanmolkapil@gmail.com>
2023-08-01 13:06:36 +0530

    Create Product component

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -4,6 +4,7 @@ import image from '../assets/linen jumpsuit.jpeg';
 const SearchResult:React.FC = () => {
   return (
     <div className='grid grid-cols-4'>
+        
         <Product productImage={image} productName='Linen jumpsuit' price={500} discountedPrice={300} stars={4} ratingCount={200}/>
 
     </div>
